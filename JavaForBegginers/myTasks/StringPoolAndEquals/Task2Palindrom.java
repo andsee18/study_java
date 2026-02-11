e91041011c2ffc6e9012013fdf23381fe8b65347
andsee18 <krllx6@mail.ru>
2026-02-12 00:40:06 +0300

    completing tasks

---
+++ JavaForBegginers/myTasks/StringPoolAndEquals/Task2Palindrom.java
@@ -0,0 +1,20 @@
+package myTasks.StringPoolAndEquals;
+
+import java.util.Scanner;
+
+public class Task2Palindrom {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPalindrome(String string){
+        String s = string.toLowerCase();
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            if (s.charAt(2))
+
+
+        }
+    }
+}
